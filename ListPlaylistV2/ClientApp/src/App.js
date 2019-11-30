75922f0fee49740f83ede1800ceff5764324c5a2
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2019-11-30 20:10:00 +0200

    Add frontend

--- ListPlaylistV2/ClientApp/src/App.js
+++ ListPlaylistV2/ClientApp/src/App.js
@@ -1,14 +1,56 @@
-﻿import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import React from 'react';
+import './App.css';
+import NavigationBar from './components/header/navbar';
+import headphones from './assets/images/headphones.svg';
+import spotifyLogo from './assets/images/spotify-logo.svg';
+import googleLogo from './assets/images/google-logo.svg';
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+function App() {
+	return (
+		<div className="App">
+			<NavigationBar />
+			<main>
+				<div className="row">
+					<div className="col">
+						<img
+							src={headphones}
+							alt="headphones"
+							className="headphones-img"
+						></img>
+					</div>
+					<div className="col">
+						<div className="row">
+							<h1 className="welcome-text">
+								Transfer your playlist from Spotify to <br /> Youtube <br />
+								and vice versa!
+							</h1>
+						</div>
+						<div className="row">
+							<div className="login-button-container">
+								<button className="spotify login-btn">
+									<img
+										src={spotifyLogo}
+										alt="google-logo"
+										className="spotify-img"
+									/>
+									<span>Login with Spotify</span>
+								</button>
+
+								<button className="google login-btn">
+									<img
+										src={googleLogo}
+										alt="spotify-logo"
+										className="google-img"
+									/>
+									<span>Login with Google</span>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
