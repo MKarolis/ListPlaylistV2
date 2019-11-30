bb4747d995a954c8c2bb9f380f55c4be2e164570
Karolis Medekša <kreisis4@gmail.com>
2019-11-30 21:41:47 +0200

    General changes

--- ListPlaylistV2/frontend/src/components/header/navbar.js
+++ ListPlaylistV2/frontend/src/components/header/navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 import logo from './../../assets/images/logo.svg';
-import './navbar.css';
 
 export const navbar = () => (
-	<Navbar expand="lg" className="navbar">
-		<Navbar.Brand href="/">
-			<img
-				src={logo}
-				className="d-inline-block align-top logo"
-				alt="List Playlist logo"
-			/>
-		</Navbar.Brand>
-	</Navbar>
+    <Navbar expand="lg" className="navbar">
+        <Navbar.Brand href="/">
+            <img
+                src={logo}
+                className="d-inline-block align-top logo"
+                alt="ListPlaylist logo"
+            />
+        </Navbar.Brand>
+        <Navbar.Text className="logged-in-as-label">
+            Hello, guest
+        </Navbar.Text>
+    </Navbar>
 );
 
 export default navbar;
