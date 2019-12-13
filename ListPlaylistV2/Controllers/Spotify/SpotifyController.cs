b6802f7db68c46157879f82213d3043cb84bc730
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2019-12-14 01:48:44 +0200

    Fuck Microsoft's JsonSerializer

--- ListPlaylistV2/Controllers/Spotify/SpotifyController.cs
+++ ListPlaylistV2/Controllers/Spotify/SpotifyController.cs
@@ -9,11 +9,12 @@ using Microsoft.AspNetCore.Mvc;
 using SpotifyAPI.Web;
 using SpotifyAPI.Web.Models;
 using SpotifyAPI.Web.Auth;
-using System.Text.Json;
 using System.Text.Json.Serialization;
 using Unosquare.Swan;
 using ListPLaylistV2.Models.Spotify.Mappers;
 using ListPLaylistV2.Models.Spotify;
+using Newtonsoft.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 // Catch 500 sometime later
 // Check if token has expired 
@@ -26,42 +27,54 @@ namespace ListPLaylistV2.Controllers.Spotify
     {
         private SpotifyWebAPI _spotify;
 
+        private Tuple<string, string> GetAuthSeparated(String Authorization)
+        {
+            int separatorIndex = Authorization.IndexOf(' ');
+            string tokenType = Authorization.Substring(0, separatorIndex);
+            string accessToken = Authorization.Substring(separatorIndex + 1);
+
+            return new Tuple<string, string>(tokenType, accessToken);
+        }
+
         [HttpGet("playlists")]
         // [Authorize]
-        public IActionResult GetPlaylists([FromHeader] string accessToken, [FromHeader] string tokenType)
+        //public IActionResult GetPlaylists([FromHeader] string accessToken, [FromHeader] string tokenType)
+        public IActionResult GetPlaylists([FromHeader] string Authorization)
         {
+            Tuple<string, string> auth = GetAuthSeparated(Authorization);
+
             _spotify = new SpotifyWebAPI()
             {
-                AccessToken = accessToken,
-                TokenType = tokenType // Always "Bearer"
+                TokenType = auth.Item1,
+                AccessToken = auth.Item2
             };
 
             String userId = _spotify.GetPrivateProfileAsync().Result.Id;
-
             Task<Paging<SimplePlaylist>> playlists = _spotify.GetUserPlaylistsAsync(userId);
 
             if (playlists.Await().HasError())
                 return BadRequest(playlists.Exception.Message);
 
-            return Ok(JsonSerializer.Serialize<List<SimplePlaylist>>(playlists.Result.Items));
+            return Ok(JsonConvert.SerializeObject(playlists.Result.Items));
         }
 
         [HttpGet("tracks")]
         // [Authorize]
-        public IActionResult GetSongs([FromHeader] string accessToken, [FromHeader] string tokenType, [FromHeader] string playlistId)
+        public IActionResult GetSongs([FromHeader] string Authorization, [FromHeader] string playlistId)
         {
+            Tuple<string, string> auth = GetAuthSeparated(Authorization);
             _spotify = new SpotifyWebAPI()
             {
-                AccessToken = accessToken,
-                TokenType = tokenType // Always "Bearer"
+                TokenType = auth.Item1,
+                AccessToken = auth.Item2
             };
-            
+
             Task<Paging<PlaylistTrack>> tracks = _spotify.GetPlaylistTracksAsync(playlistId);
 
             if (tracks.Await().HasError())
                 return BadRequest();
 
-            return Ok(JsonSerializer.Serialize<List<SpotifyTrack>>(tracks.Result.Items.Select(track => SpotifyTrackMapper.map(track)).ToList()));
+            return Ok(JsonConvert.SerializeObject(tracks.Result.Items.Select(track => SpotifyTrackMapper.map(track)).ToList()));
         }
 
         // POST: api/Spotify
