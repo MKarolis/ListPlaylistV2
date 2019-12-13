587dcfd4e9ca8aae4d4aa441ef883a7b6fb49973
Adomas <A.Naus@student.tudelft.nl>
2019-12-13 19:47:15 +0200

    added images

--- ListPlaylistV2/frontend/src/containers/SourceSelect/SourceSelect.js
+++ ListPlaylistV2/frontend/src/containers/SourceSelect/SourceSelect.js
@@ -2,37 +2,41 @@
 import { connect } from 'react-redux';
 
 import './SourceSelect.css';
-import { spotifyLogoBlue } from '../../assets/images/spotify-blue.svg';
-import { youtubeLogoBlue } from '../../assets/images/youtube-blue.svg';
-
+import spotifyLogoBlue from '../../assets/images/spotify-blue.svg';
+import youtubeLogoBlue from '../../assets/images/youtube-blue.svg';
 
 class SourceSelect extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return (
-            <React.Fragment>
-            <div className='row'>
-                <h2 className='center-heading heading-big'>Select playlist <br/> source</h2>
-            </div>
-            <div className='row'>
-                    <div className='col-md-6 com-sm-12'>
-                        <a><img src = ''/></a>
-                    </div>
-                    <div className='col-md-6 com-sm-12'>
-                        Second
-                    </div>
-            </div>
-            </React.Fragment>
-        );
-    }
-
+	render() {
+		return (
+			<React.Fragment>
+				<div className="row">
+					<h2 className="center-heading heading-big">
+						Select playlist <br /> source
+					</h2>
+				</div>
+				<div className="row mx-auto d-inline-flex mt-5">
+					<div className="col-md-6 com-sm-12">
+						<a href="">
+							<img src={spotifyLogoBlue} className="spotify-logo-blue" />
+						</a>
+					</div>
+					<div className="col-md-6 com-sm-12">
+						<a href="">
+							<img src={youtubeLogoBlue} className="spotify-logo-blue" />
+						</a>
+					</div>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({
-    authenticatedWithGoogle: state.authentication.authenticatedWithGoogle
+const mapStateToProps = state => ({
+	authenticatedWithGoogle: state.authentication.authenticatedWithGoogle
 });
 
 export default connect(mapStateToProps)(SourceSelect);
