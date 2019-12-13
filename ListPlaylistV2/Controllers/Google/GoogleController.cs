8d9b524a64f1d349a8a51881d740dea7a6b30221
Vladas Zvonkus <vladas.zvonkus@gmail.com>
2019-12-14 00:43:40 +0200

    Fix controller routing

--- ListPlaylistV2/Controllers/Google/GoogleController.cs
+++ ListPlaylistV2/Controllers/Google/GoogleController.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Google.Apis.Services;
+using Google.Apis.Upload;
+using Google.Apis.Util.Store;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 
 namespace ListPLaylistV2.Controllers.Google
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class GoogleController : ControllerBase
     {
+        
+        [HttpGet]
+        public String GetTest()
+        {
+            return "Test";
+            
+        }
     }
 }
