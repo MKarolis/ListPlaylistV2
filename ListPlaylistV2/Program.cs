fc40c0612cce8424b2fd513243562ede5898ec5e
Matas <matas.remeskevicius@gmail.com>
2019-12-13 17:22:39 +0200

    Auto stash before merge of "Authentication" and "origin/Authentication"

--- ListPlaylistV2/Program.cs
+++ ListPlaylistV2/Program.cs
@@ -4,8 +4,13 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ListPLaylistV2
@@ -19,6 +24,51 @@ namespace ListPLaylistV2
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>();
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddMvc(options => { options.EnableEndpointRouting = false; }).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            services.AddIdentity<IdentityUser, IdentityRole>();
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = GoogleDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+            })
+                    .AddGoogle(options =>
+                    {
+                        options.ClientId = "[709242795506-2i1012a78mitl16o3cejahdjn5r93s0q.apps.googleusercontent.com]";
+                        options.ClientSecret = "[VxxDypG9NziMbD6HwlLPlv-W]";
+                    });
+
+
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials());
+            app.UseAuthentication();
+            app.UseMvc();
+        }
     }
+
 }
