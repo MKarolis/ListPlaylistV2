c6ab02a06f7521c42d32df49caf9a5dbece5a5f9
Karolis Medekša <kreisis4@gmail.com>
2019-12-15 01:58:01 +0200

    Edited Spotify scope

--- ListPlaylistV2/frontend/src/config/SpotifyConfig/SpotifyConfig.js
+++ ListPlaylistV2/frontend/src/config/SpotifyConfig/SpotifyConfig.js
@@ -1,7 +1,7 @@
 
 export const scopes = ["user-read-private ",
     "user-read-email ", "playlist-read-private ",
-    "playlist-read-collaborative ", "playlist-modify-private "];
+    "playlist-read-collaborative ", "playlist-modify-private ", "playlist-modify "];
 
 export const authEndpoint = 'https://accounts.spotify.com/authorize';
 export const clientId = "2672410b3d38492198b5e62b0b736784";
