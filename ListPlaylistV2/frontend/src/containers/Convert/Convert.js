6f906f2bf39c68926a3746a158c20ff7ea38f6f8
Adomas <A.Naus@student.tudelft.nl>
2019-12-16 06:26:57 +0200

    i will now proceed to commit sepuku

--- ListPlaylistV2/frontend/src/containers/Convert/Convert.js
+++ ListPlaylistV2/frontend/src/containers/Convert/Convert.js
@@ -3,39 +3,51 @@ import { connect } from 'react-redux';
 import './Convert.css';
 import PlaylistTable from '../../components/PlaylistTable/PlaylistTable';
 import ConvertButton from '../../components/Button/Button';
-import { PLAYLIST_SOURCE_SPOTIFY, PLAYLIST_SOURCE_YOUTUBE } from '../../state/playlists/playlistsSources';
+import {
+	PLAYLIST_SOURCE_SPOTIFY,
+	PLAYLIST_SOURCE_YOUTUBE
+} from '../../state/playlists/playlistsSources';
 import * as playlistsActions from '../../state/playlists/playlistsActions';
 import LoadingWide from '../../components/LoadingWide/LoadingWide';
+import PlaylistTransferModal from '../PlaylistTransferModal/PlaylistTransferModal';
+import store from '../../state/store';
 
 class Convert extends React.Component {
 	constructor(props) {
 		super(props);
-    }
+		this.state = {
+			show: false
+		};
+	}
 
-    componentDidMount() {
-		const {
-			source, googleToken, spotifyToken, fetchPlaylists
-		} = this.props;
+	componentDidMount() {
+		const { source, googleToken, spotifyToken, fetchPlaylists } = this.props;
 
 		if (!source) {
 			this.props.history.push('/source-select');
 		}
 
-        switch (source) {
+		switch (source) {
 			case PLAYLIST_SOURCE_SPOTIFY:
-                fetchPlaylists(source, spotifyToken);
-                break;
+				fetchPlaylists(source, spotifyToken);
+				break;
 			case PLAYLIST_SOURCE_YOUTUBE:
-                fetchPlaylists(source, googleToken);
+				fetchPlaylists(source, googleToken);
 				break;
 			default:
 		}
-    }
-
+	}
+	handleClick() {
+		this.setState({
+			show: true
+		});
+		this.forceUpdate();
+		{
+			console.log(this.state.show);
+		}
+	}
 	render() {
-        const {
-             isLoading,
-        } = this.props;
+		const { isLoading } = this.props;
 		return (
 			<React.Fragment>
 				{isLoading ? (
@@ -45,12 +57,19 @@ class Convert extends React.Component {
 						<div className=" center-div">
 							<h1 className="convert-heading">Select playlist to move</h1>
 						</div>
-						<PlaylistTable/>
+						<PlaylistTable />
 						<div className="center-div high-container">
-							<ConvertButton className="cnv-btn" />
+							<ConvertButton
+								className="cnv-btn"
+								onClick={console.log(this.handleClick)}
+							/>
 						</div>
 					</div>
 				)}
+				<PlaylistTransferModal
+					open={this.state.show}
+					history={this.props.history}
+				/>
 			</React.Fragment>
 		);
 	}
@@ -61,11 +80,12 @@ const mapStateToProps = state => ({
 	spotifyToken: state.authentication.spotifyAccessToken,
 	source: state.playlists.source,
 	isLoading: state.playlists.isLoading,
-	playlists: state.playlists.playlists,
+	playlists: state.playlists.playlists
 });
 
-const mapDispatchToProps = (dispatch) => ({
-	fetchPlaylists: (source, accessToken) => dispatch(playlistsActions.fetchPlaylists(source, accessToken)),
+const mapDispatchToProps = dispatch => ({
+	fetchPlaylists: (source, accessToken) =>
+		dispatch(playlistsActions.fetchPlaylists(source, accessToken))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Convert);
--- ListPlaylistV2/frontend/src/containers/PlaylistTransferModal/PlaylistTransferModal.js
+++ ListPlaylistV2/frontend/src/containers/PlaylistTransferModal/PlaylistTransferModal.js
@@ -1,33 +1,35 @@
-import React from "react";
-import {Modal} from 'react-bootstrap'
+import React from 'react';
+import { Modal } from 'react-bootstrap';
 
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 import './PlaylistTransferModal.css';
-import ModalTransferDialog from "../../components/ModalTransferDialog/ModalTransferDialog";
-import ModalTransferSuccessDialog from "../../components/ModalTransferSuccessDialog/ModalTransferSuccessDialog";
+import ModalTransferDialog from '../../components/ModalTransferDialog/ModalTransferDialog';
+import ModalTransferSuccessDialog from '../../components/ModalTransferSuccessDialog/ModalTransferSuccessDialog';
 
-class PlaylistTransferModal extends React.Component{
-    constructor(props){
-        super(props);
-    }
+class PlaylistTransferModal extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        const {finishedConverting} = this.props;
-        return(
-            <Modal show={this.props.show} onHide={this.props.onHide} size="xl" centered>
-                <Modal.Body className="migration-modal-body">
-                    {!finishedConverting ?
-                        <ModalTransferDialog/> :
-                        <ModalTransferSuccessDialog history={this.props.history}/>}
-                </Modal.Body>
-            </Modal>
-        );
-    }
+	render() {
+		const { finishedConverting } = this.props;
+		return (
+			<Modal show={this.isShown} onHide={this.props.onHide} size="xl" centered>
+				<Modal.Body className="migration-modal-body">
+					{!finishedConverting ? (
+						<ModalTransferDialog show={this.props.isShown} />
+					) : (
+						<ModalTransferSuccessDialog history={this.props.history} />
+					)}
+				</Modal.Body>
+			</Modal>
+		);
+	}
 }
 
-const mapStateToProps = (state) => ({
-   finishedConverting: state.migration.playlistConverted
+const mapStateToProps = state => ({
+	finishedConverting: state.migration.playlistConverted
 });
 
 export default connect(mapStateToProps)(PlaylistTransferModal);
