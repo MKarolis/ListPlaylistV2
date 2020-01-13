b12cc4a0be9b625bf4e093c16518827288a2beda
Adomas <A.Naus@student.tudelft.nl>
2020-01-13 18:05:35 +0200

    changes of extreme importance
    need to change button font size ant letter spacing

--- ListPlaylistV2/frontend/src/containers/GlobalErrorBoundary/GlobalErrorBoundary.js
+++ ListPlaylistV2/frontend/src/containers/GlobalErrorBoundary/GlobalErrorBoundary.js
@@ -3,43 +3,42 @@ import './GlobalErrorBoundary.css';
 import { connect } from 'react-redux';
 
 class GlobalErrorBoundary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false };
-    }
+	constructor(props) {
+		super(props);
+		this.state = { hasError: false };
+	}
 
-    componentDidCatch(error, info) {
-        this.setState({ hasError: true });
-        // log here
-    }
+	componentDidCatch(error, info) {
+		this.setState({ hasError: true });
+		// log here
+	}
 
-    render() {
-        const {
-            hasErrorPlaylists, hasErrorMigration
-        } = this.props;
+	render() {
+		const { hasErrorPlaylists, hasErrorMigration } = this.props;
 
-        if (this.state.hasError || hasErrorPlaylists || hasErrorMigration) {
-            this.props.history.push("/");
-            return (
-                <div className="error-text-div">
-                    <h1 className="error-text">Something went wrong :(</h1>
-                </div>
-            );
-        }
+		if (this.state.hasError || hasErrorPlaylists || hasErrorMigration) {
+			this.props.history.push('/');
+			return (
+				<div className="error-text-div">
+					<h1 className="error-text">Something went wrong :(</h1>
+				</div>
+			);
+		}
 
-        console.log("wut");
+		console.log('wut');
 
-        return this.props.children;
-    }
+		return this.props.children;
+	}
 }
 
 const mapStateToProps = state => ({
-    hasErrorPlaylists: state.playlists.hasError,
-    hasErrorMigration: state.migration.hasError,
+	hasErrorPlaylists: state.playlists.hasError,
+	hasErrorMigration: state.migration.hasError
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = dispatch => ({});
 
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(GlobalErrorBoundary);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(GlobalErrorBoundary);
